6f93c4009d9219f0947f8bc81a066a0bdf417848
Rawan <https://github.com/wawooona/SwiftUI.git>
2020-05-03 23:30:13 +0300

    Initial Commit

---
+++ Hw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hw1
+//
+//  Created by Rawan Alhammadi on 5/3/20.
+//  Copyright © 2020 Rawan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
